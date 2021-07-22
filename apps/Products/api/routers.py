212b98ebce18530f6a76b63bc54b2e73eb6688df
Maicol Rodrigo <maicolcrodrigoa@gmail.com>
2021-07-22 16:20:21 -0500

    temp

--- apps/Products/api/routers.py
+++ apps/Products/api/routers.py
@@ -2,6 +2,7 @@ from rest_framework.routers import DefaultRouter
 from apps.Products.api.views.product_views import ProductViewSet
 from apps.Products.api.views.general_views import MeasureUnitViewSet
 
+
 router = DefaultRouter()
 router.register(r'Products',ProductViewSet,basename = 'product')
 router.register(r'MeasureUnit',MeasureUnitViewSet,basename = 'unit')
--- settings.py
+++ settings.py
@@ -37,11 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
 
     # CORS HEADERS
     'corsheaders',
-
+    
     'drf_yasg',
     'rest_framework',
     'rest_framework.authtoken',
@@ -54,7 +53,7 @@ INSTALLED_APPS = [
     'apps.Products',
 ]
 
-TOKEN_EXPIRED_AFTER_SECONDS = 50000000
+TOKEN_EXPIRED_AFTER_SECONDS = 10
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -160,23 +159,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS HEADERS
 CORS_ORIGIN_ALLOW_ALL = True
-
-CORS_ALLOW_METHODS = [
-'DELETE',
-'GET',
-'OPTIONS',
-'PATCH',
-'POST',
-'PUT',
-]
-CORS_ALLOW_HEADERS = [
-'accept',
-'accept-encoding',
-'authorization',
-'content-type',
-'dnt',
-'origin',
-'user-agent',
-'x-csrftoken',
-'x-requested-with',
-]
