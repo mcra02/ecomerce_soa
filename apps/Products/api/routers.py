445bb1f4e6644bfd159a6a0b4e7943d0ad3bb2c5
bcalisaya4 <u17200830@utp.edu.pe>
2021-07-22 16:02:22 -0500

    comentario unidades

--- apps/Products/api/routers.py
+++ apps/Products/api/routers.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from apps.Products.api.views.product_views import ProductViewSet
-#from apps.Products.api.views.general_views import MeasureUnitViewSet
+from apps.Products.api.views.general_views import MeasureUnitViewSet
 
 router = DefaultRouter()
 router.register(r'Products',ProductViewSet,basename = 'product')
